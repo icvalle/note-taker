ba947111f720df47ab60322dab1aa9948b080954
Iris Valle <iris.valle.design@gmail.com>
2021-05-04 22:52:31 -0400

    adding file structure, server.js, started js routes, added a Notes class

---
+++ routes/apiRoutes.js
@@ -0,0 +1,12 @@
+module.exports = (app) => {
+    // app.get('/api/notes', (req, res) => {
+    //     res.json(db.json);
+    // });
+    
+    // app.post('/api/notes', (req, res) => {
+    //     const newNote = req.body;
+    //     newNote.routeName = newNote.name.replace(/\s+/g, '');
+    //     notes.push(newNote);
+    //     res.json(newNote);
+    // });
+};
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => console.log(`Listening on PORT: http://localhost:${PORT}`));
