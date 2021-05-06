e0d22b27f0b31b40cf69f946bdf999040f6aac7f
Iris Valle <iris.valle.design@gmail.com>
2021-05-05 23:41:04 -0400

    updated apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,14 @@
+const db = require('../db/db.json');
+const Notes = require('../db/notes.js');
+
 module.exports = (app) => {
-    // app.get('/api/notes', (req, res) => {
-    //     res.json(db.json);
-    // });
+    app.get('/api/notes', (req, res) => {
+        res.json(db);
+    });
     
-    // app.post('/api/notes', (req, res) => {
-    //     const newNote = req.body;
-    //     newNote.routeName = newNote.name.replace(/\s+/g, '');
-    //     notes.push(newNote);
-    //     res.json(newNote);
-    // });
+    app.post('/api/notes', (req, res) => {
+        const newNote = new Notes(req.body.title, req.body.text)
+        db.push(newNote);
+        res.json(true);
+    });
 };
