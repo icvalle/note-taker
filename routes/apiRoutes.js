2fe24eb1a5cccdacd78a2dac108c76a0601e9795
Iris Valle <iris.valle.design@gmail.com>
2021-05-06 22:33:22 -0400

    added uuid to generate id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const db = require('../db/db.json');
 const Notes = require('../db/notes.js');
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => {
@@ -7,7 +8,7 @@ module.exports = (app) => {
     });
     
     app.post('/api/notes', (req, res) => {
-        const newNote = new Notes(req.body.title, req.body.text, 1);
+        const newNote = new Notes(req.body.title, req.body.text, uuidv4());
         db.push(newNote);
         res.json(true);
     });
